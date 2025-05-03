672c679d0dc5a69934b28291cd29fc889a1a087e
Gorilla1206 <rosius.daan@gmail.com>
2025-05-03 10:45:37 +0200

    python 2 les 7
    mejhfLHGfeosqibiqygigigiyygoiygiogioygsioygigyoiqgoiygiargriyoaoigygiygiyagygyg'jgthmothmouzghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiittttttttttttttttttttttttttttttttttttttttttttttttttttttttllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

---
+++ startcode/sneeuwvlok.py
@@ -0,0 +1,14 @@
+import turtle
+class Koch:
+    def__init__(self, start_x, start_y):
+    self.pen = turtle.Turtle()
+    self.pen.speed(0)
+    self.pen.penup( )
+    self.pen.goto(start_x, start_y)
+    self.pen.pendown()
+
+  defteken(self.orde, lengte):
+if orde == 0
+
+else:nieuwe_lengte lengt/3
+self.te
